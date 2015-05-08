ef3cb28e889b4826e743480d729e81f3e88645c3
desire <dperraultreid@gmail.com>
2015-05-08 22:12:04 +0000

    initialized with stylist_id

--- lib/client.rb
+++ lib/client.rb
@@ -1,9 +1,9 @@
 class Client
-  attr_reader(:name, :id)
+  attr_reader(:name, :stylist_id)
   
   define_method(:initialize) do |attributes|
     @name = attributes.fetch(:name)
-    @id = attributes.fetch(:id)
+    @stylist_id = attributes.fetch(:stylist_id)
   end 
 
   define_singleton_method(:all) do
@@ -11,19 +11,18 @@ class Client
     clients = []
     returned_clients.each() do |client|
       name = client.fetch("name")
-      id = client.fetch("id").to_i()
-      clients.push(Client.new({:name => name, :id => id}))
+      stylist_id = client.fetch("stylist_id").to_i()
+      clients.push(Client.new({:name => name, :stylist_id => stylist_id}))
     end
     clients
   end
   
   define_method(:save) do
-    result = DB.exec("INSERT INTO clients (name) VALUES ('#{@name}') RETURNING id;")
-    @id = result.first().fetch("id").to_i()
+    DB.exec("INSERT INTO clients (name, stylist_id) VALUES ('#{@name}', #{@stylist_id});")
   end
 
   define_method(:==) do |another_client|
-    self.name().==(another_client.name()).&(self.id().==(another_client.id()))
+    self.name().==(another_client.name()).&(self.stylist_id().==(another_client.stylist_id()))
   end
   
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -2,30 +2,7 @@ require("spec_helper")
 
 describe(Client) do
   
-  describe("#==") do
-    it("is the same client if they have the same name") do
-      test_client_1 = Client.new({:name => "Angela Moore", :id => nil})
-      test_client_2 = Client.new({:name => "Angela Moore", :id => nil})
-      expect(test_client_1).to(eq(test_client_2))
-    end
-  end
-  
-  describe("#name")do 
-    it("gives you the name of the client") do
-      test_client = Client.new({:name => "Angela Moore", :id => nil})
-      expect(test_client.name()).to(eq("Angela Moore"))
-    end
-  end
-  
-    describe("#id") do
-      it("sets the ID for the client when saved") do
-        client = Client.new({:name => "Angela Moore", :id => nil})
-        client.save()
-        expect(client.id()).to(be_an_instance_of(Fixnum))
-    end
-  end
-
-  
+   
   describe(".all") do
     it("is starts without any clients") do
       expect(Client.all()).to(eq([]))
@@ -34,13 +11,42 @@ describe(Client) do
   
   describe("#save") do
     it("add a client to the array of saved clients") do
-      test_client = Client.new({:name => "Angela Moore", :id => nil})
+      test_client = Client.new({:name => "Angela Moore", :stylist_id => 1})
       test_client.save()
       expect(Client.all()).to(eq([test_client]))
     end
   end
- 
+  
+  describe("#==") do
+    it("is the same client if they have the same name and stylist id") do
+      test_client_1 = Client.new({:name => "Angela Moore", :stylist_id => 1})
+      test_client_2 = Client.new({:name => "Angela Moore", :stylist_id => 1})
+      expect(test_client_1).to(eq(test_client_2))
+    end
+  end
+  
+  describe("#name")do 
+    it("gives you the name of the client") do
+      test_client = Client.new({:name => "Angela Moore", :stylist_id => nil})
+      expect(test_client.name()).to(eq("Angela Moore"))
+    end
+  end
+  
+  #  describe("#id") do
+   #   it("sets the ID for the client when saved") do
+      #  client = Client.new({:name => "Angela Moore", :id => nil})
+   #     client.save()
+      #  expect(client.id()).to(be_an_instance_of(Fixnum))
+   # end
+  #end
 
+  describe("#styist_id") do
+    it("lets you read the stylist id out") do
+    test_client = Client.new({:name => "Angela Moore", :stylist_id => 1})
+    expect(test_client.stylist_id()).to(eq(1))
+    end
+  end
+ 
   
 
 end
